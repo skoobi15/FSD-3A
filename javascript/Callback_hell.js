855283a4987c905f9f363d5e2c5bdf3072946709
skoobi15 <ananyaaa171@gmail.com>
2026-09-03 13:51:11 +0530

    c

--- javascript/Callback_hell.js
+++ javascript/Callback_hell.js
@@ -1,14 +1,25 @@
-function test1() {
+function test1(cb) {
     setTimeout(() =>{
         console.log("task1");
+        cb();
     },4000)
 }
 
-function test2() {
+function test2(cb) {
     setTimeout(() =>{
         console.log("task2");
+        cb();
     },2000)
 }
+function test3(cb) {
+    setTimeout(() =>{
+        console.log("task3");
+        cb();
+    },200)
+}
 
-test1();
-test2();
+test1(()=>{
+    test2(()=>{
+        test3()
+    });
+})
