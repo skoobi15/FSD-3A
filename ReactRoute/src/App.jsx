0f11875b9d0c92a167d9f1bdb7a710e7eb73723e
skoobi15 <ananyaaa171@gmail.com>
2026-09-24 14:05:05 +0530

    commit

--- ReactRoute/src/App.jsx
+++ ReactRoute/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 function Home() {
   return  <h1>This is my Home Page</h1>
 }
@@ -11,9 +11,15 @@ const App = () => {
     <div>
       <BrowserRouter>
       <nav>
-        <Link to="/">HOME</Link>
+        <Link to="/">HOME </Link>
         <Link to="/about">ABOUT US</Link>
       </nav>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+
+        
+      </Routes>
       </BrowserRouter>
     </div>
   )
